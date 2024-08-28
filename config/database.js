55e97692e48f54890db9269d08d21074dff39901
Marcus Bragantini <bragantini34@gmail.com>
2024-08-28 20:44:05 -0300

    Modificação para produção

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('gerenciador_veiculos', 'root', 'Bragantini', {
-    host: '127.0.0.1',
+const sequelize = new Sequelize('u950457610_veiculos','u950457610_becape',"Mvb081521", {
+    host: '149.62.37.1',
     dialect: 'mysql',
     logging: false, // Desativa logs de SQL no console
     define: {
