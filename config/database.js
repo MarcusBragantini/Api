efeae5a144bbeeb99355d7be83fa796b2aa0ee23
Marcus Bragantini <111925312+MarcusBragantini@users.noreply.github.com>
2024-08-29 10:35:45 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,12 +1,20 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('gerenciador_veiculos', 'root', 'Bragantini', {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    logging: false, // Desativa logs de SQL no console
-    define: {
-        timestamps: false // Desativa timestamps automáticos
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        logging: false, 
+        define: {
+            timestamps: false
+        }
     }
-});
+);
 
 export default sequelize;
